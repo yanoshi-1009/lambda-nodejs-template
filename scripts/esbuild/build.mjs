fa3142ca28bae210378ed6a35cbfc59ac12a91dd
yanoshi-1009 <y.idakko@gmail.com>
2026-03-26 11:09:20 +0900

    chore: update TypeScript target to esnext and adjust esbuild configuration

--- scripts/esbuild/build.mjs
+++ scripts/esbuild/build.mjs
@@ -3,6 +3,7 @@ import * as esbuild from "esbuild";
 const context = await esbuild.context({
   entryPoints: ["./src/index.ts"],
   platform: "node",
+  target: "node24",
   minify: true,
   bundle: true,
   format: "esm",
